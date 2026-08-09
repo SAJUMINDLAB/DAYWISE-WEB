a50b5b4a940471825d35b043e1b2c1c96f4e15cc
SAJUMINDLAB <yudhyun01228@gmail.com>
2026-08-09 23:00:11 +0900

    Update useKakaoShare.js

--- src/hooks/useKakaoShare.js
+++ src/hooks/useKakaoShare.js
@@ -10,8 +10,12 @@ export const useKakaoShare = () => {
         window.Kakao.init(KAKAO_KEY); 
       }
       try {
-        // shareUrl도 localhost일 경우 Vercel 도메인으로 강제 치환
-        const safeShareUrl = shareUrl.replace(/http:\/\/localhost:\d+/, PRODUCTION_DOMAIN);
+        // 어떤 환경(테스트 도메인 등)에서 테스트하더라도 카카오에 등록된 정식 도메인으로 강제 고정
+        const urlObj = new URL(shareUrl, PRODUCTION_DOMAIN);
+        urlObj.hostname = 'daywise-web-six.vercel.app';
+        urlObj.protocol = 'https:';
+        // 카카오 스크랩 캐시를 우회하기 위해 (쿼리파라미터를 무시하므로) 무의미한 쿼리 파라미터라도 일단 붙여둠
+        const safeShareUrl = urlObj.toString();
 
         window.Kakao.Share.sendScrap({
           requestUrl: safeShareUrl
